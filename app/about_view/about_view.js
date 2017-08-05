8f860e991c6934feb0aa0a8a715c19229e46894d
Leonard Grazian <grazian.leonard@gmail.com>
2017-08-05 10:13:21 -0400

    added other pages

---
+++ app/about_view/about_view.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('aboutView', ['ngRoute'])
+  .controller('aboutCtrl', [function () {
+    
+  }]);
---
+++ app/login_view/login_view.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('appView', ['ngRoute'])
+  .controller('loginCtrl', [function() {
+
+  }]);
